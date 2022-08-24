9eb38667854a447e468b6853f2a2ad3bf4c09104
Ranjan <sahaadi99@gmail.com>
2022-08-24 15:34:12 +0530

    First commit

---
+++ src/test/java/com/test/dockerise/Course1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.dockerise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
